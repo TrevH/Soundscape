5bda84a6e7fa01448c5f1715b3d6cbfb14d3f4c8
Pete Worthy <p.worthy@uq.edu.au>
2017-11-20 15:32:05 +1000

    Cleaning and commenting ss.js

--- js/ss.js
+++ js/ss.js
@@ -1,5 +1,6 @@
 $(document).ready(function()
 {    
+	// this block happens when the page first loads up
 	var pageselected = localStorage.getItem('selectedPage');
 	console.log(pageselected);
 
@@ -12,11 +13,16 @@ $(document).ready(function()
 		$('#content-section-block').load(pageselected + '.html');
 	}
 	
-
+	// this block happens only when a .page2-menu-options element is clicked
 	$('.page2-menu-options').on('click', function(e)
 	{
         e.preventDefault();
 		var page = $(this).attr('href');
+		
+		// this will update localStorage with the last page clicked
+		// if a person navigates away and then clicks back it will
+		// open the last page they were on
+		localStorage.setitem("selectedPage", page);
 
 		if(page == 'report_shiva' || page == 'report_trevor')
 		{
